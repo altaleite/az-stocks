bc154af0eecf285c2234aa308c46075315683e12
Rafael Azevedo <rafaelzooufmg@gmail.com>
2026-06-17 17:52:24 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,6 @@
 
 window.AZ_CONFIG = {
   GOOGLE_SHEET_ID: "18FeqyS47TafLb4IXozbKBuCw_FtbbSxFac9Qn7x-OLs",
-  SHEET_NAME: "Dados"
+  SHEET_NAME: "Dados",
+  GOOGLE_SHEET_URL: "https://docs.google.com/spreadsheets/d/18FeqyS47TafLb4IXozbKBuCw_FtbbSxFac9Qn7x-OLs/edit?gid=1857073884#gid=1857073884"
 };
